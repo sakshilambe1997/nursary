9145b2d30efe7c7c0b554326574af0e17aa1194f
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-08-16 09:59:05 +0530

    Delete Api is added

--- index.js
+++ index.js
@@ -5,33 +5,33 @@ app.use(express.json());
 
 const plants = [
   {
-   "id": 1,
-    "name": "Bamboo",
-    "category": "indoor",
-    "image":
+    id: 1,
+    name: "Bamboo",
+    category: "indoor",
+    image:
       "https://www.ugaoo.com/cdn/shop/files/Aimage_3.jpg?v=1682523121&width=750",
-    "price": "1500",
-    "description": "Lucky Bamboo Plant - 3 Layer",
+    price: "1500",
+    description: "Lucky Bamboo Plant - 3 Layer",
   },
 
   {
-    "id": 2,
-    "name": "Hibiscus",
-    "category": "indoor",
-    "image":
+    id: 2,
+    name: "Hibiscus",
+    category: "indoor",
+    image:
       "https://upload.wikimedia.org/wikipedia/commons/c/cb/Hibiscus_flower_TZ.jpg",
-    "price": "250",
-    "description": "The Hibiscus plant",
+    price: "250",
+    description: "The Hibiscus plant",
   },
 
   {
-    "id": 3,
-    "name": "Rose",
-    "category": "outdoor",
-    "image":
+    id: 3,
+    name: "Rose",
+    category: "outdoor",
+    image:
       "https://media.istockphoto.com/id/1256125259/photo/pink-roses-in-garden.jpg?s=1024x1024&w=is&k=20&c=kkc2SYt14-gmJ5Lb068DlnKS5q5A7H-SYmM9OeSV26o=",
-    "price": "2000",
-    "description": "Lucky Rose Plant - 6 Layer",
+    price: "2000",
+    description: "Lucky Rose Plant - 6 Layer",
   },
 ];
 
@@ -98,7 +98,6 @@ app.post("/plant", (req, res) => {
   });
 });
 
-
 app.get("/plants", (req, res) => {
   res.json({
     sucess: true,
@@ -107,54 +106,71 @@ app.get("/plants", (req, res) => {
   });
 });
 
-app.put("/plant/:id",(req,res)=>{
-const { name,
-   category, 
-   image,
-    price,
-     description } = req.body;
-    const { id }= req.params;
+app.put("/plant/:id", (req, res) => {
+  const { name, category, image, price, description } = req.body;
+  const { id } = req.params;
 
-    let index=-1;
+  let index = -1;
 
-   plants.forEach((plant,i)=>{
-    if(plant.id==id){
-        index=i
+  plants.forEach((plant, i) => {
+    if (plant.id == id) {
+      index = i;
     }
-   })
+  });
 
-   const newObj = {
+  const newObj = {
     id,
     name,
     category,
     image,
     price,
-    description
-   }
+    description,
+  };
 
-   if(index==-1){
+  if (index == -1) {
     res.json({
-        sucess:false,
-        data:null,
-        message:`plant not found for id ${id}`
-    })
-   }
-
-   else{
-    plants[index]= newObj
+      sucess: false,
+      data: null,
+      message: `plant not found for id ${id}`,
+    });
+  } else {
+    plants[index] = newObj;
 
     res.json({
-        sucess:true,
-        data:newObj,
-        message:`plant updated successfully`
-    })
+      sucess: true,
+      data: newObj,
+      message: `plant updated successfully`,
+    });
+  }
+});
 
-   }
+app.delete("/plant/:id",(req,res)=>{
+  const {id}= req.params
 
-   
+  let index=-1
+
+  plants.forEach((plant,i)=>{
+  if(plant.id==id){
+    index= i
+  }
+  })
+
+  if(index==-1){
+  res.json({
+    success:false,
+    messsage:`Plant not found id ${id}`,
+    data:null
+  })
+}
+
+plants.splice(index,1)
+
+  res.json({
+    success:true,
+    messsage:`Plant deleted successfully`,
+    data:null
+  })
 })
-
-
 
 const PORT = 5000;
 app.listen(PORT, () => {
