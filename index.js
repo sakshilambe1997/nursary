a1c567e0b8c9e40bedb3194187d27810c706583f
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-08-17 17:34:23 +0530

    mongodb connection added

--- index.js
+++ index.js
@@ -1,73 +1,91 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
+
 const app = express();
 app.use(express.json());
 
-import {getHealth} from "./controllers/health.js";
-import {postPlant,getPlants,getPlantId,putPlantId,deletePlantId} from "./controllers/plant.js"
-import {handlePageNotFound404} from "./controllers/errors.js"
+const DbConnection = async (req, res) => {
+  const conn = await mongoose.connect(process.env.MONGO_URL);
+
+  if(conn){
+    console.log("MongoDB Connected Successfully")
+  }
+  else{
+    console.log("MongoDB  Not Connected")
+  }
+};
+DbConnection();
+
+
+import { getHealth } from "./controllers/health.js";
+import {
+  postPlant,
+  getPlants,
+  getPlantId,
+  putPlantId,
+  deletePlantId,
+} from "./controllers/plant.js";
+import { handlePageNotFound404 } from "./controllers/errors.js";
 
 const plants = [
   {
-    "id": 1,
-    "name": "Bamboo",
-    "category": "indoor",
-    "image":
+    id: 1,
+    name: "Bamboo",
+    category: "indoor",
+    image:
       "https://www.ugaoo.com/cdn/shop/files/Aimage_3.jpg?v=1682523121&width=750",
-    "price": "1500",
-    "description": "Lucky Bamboo Plant - 3 Layer",
+    price: "1500",
+    description: "Lucky Bamboo Plant - 3 Layer",
   },
 
   {
-    "id": 2,
-    "name": "Hibiscus",
-    "category": "indoor",
-    "image":
+    id: 2,
+    name: "Hibiscus",
+    category: "indoor",
+    image:
       "https://upload.wikimedia.org/wikipedia/commons/c/cb/Hibiscus_flower_TZ.jpg",
-    "price": "250",
-    "description": "The Hibiscus plant",
+    price: "250",
+    description: "The Hibiscus plant",
   },
 
   {
-    "id": 3,
-    "name": "Rose",
-    "category": "outdoor",
-    "image":
+    id: 3,
+    name: "Rose",
+    category: "outdoor",
+    image:
       "https://media.istockphoto.com/id/1256125259/photo/pink-roses-in-garden.jpg?s=1024x1024&w=is&k=20&c=kkc2SYt14-gmJ5Lb068DlnKS5q5A7H-SYmM9OeSV26o=",
-    "price": "2000",
-    "description": "Lucky Rose Plant - 6 Layer",
+    price: "2000",
+    description: "Lucky Rose Plant - 6 Layer",
   },
 
   {
-    "id": 3,
-    "name" :"flower",
-    "category":"outdoor",
-    "image": "https://www.ugaoo.com/cdn/shop/files/Aimage_3.jpg?v=1682523121&width=750",
-    "price": "1000",
-    "description":"Lucky Sunflower Plant"
-
-}
-
-
+    id: 3,
+    name: "flower",
+    category: "outdoor",
+    image:
+      "https://www.ugaoo.com/cdn/shop/files/Aimage_3.jpg?v=1682523121&width=750",
+    price: "1000",
+    description: "Lucky Sunflower Plant",
+  },
 ];
 
-   
-app.get("/health",getHealth)
+app.get("/health", getHealth);
 
-app.post("/plant",postPlant )
+app.post("/plant", postPlant);
 
 app.get("/plants", getPlants);
 
-app.get("/plant/:id",getPlantId);
+app.get("/plant/:id", getPlantId);
 
 app.put("/plant/:id", putPlantId);
 
-app.delete("/plant/:id",deletePlantId)
+app.delete("/plant/:id", deletePlantId);
 
-app.use("*",handlePageNotFound404)
+app.use("*", handlePageNotFound404);
 
- const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 });
